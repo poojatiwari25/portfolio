cdcae0d9223f9969b98c35d39f7d2548c0dbfafd
Pooja Tiwari <poojatiwari2508@gmail.com>
2025-01-08 01:01:00 +0530

    new changes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,4 +1,4 @@
-'use client'; // Add this to mark the component as client-side
+'use client'; 
 
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
@@ -8,31 +8,31 @@ export default function Hero() {
   const texts = ['Pooja Tiwari', 'Frontend Developer', 'Programmer', 'Software Engineer'];
   const [textIndex, setTextIndex] = useState(0);
   const [charIndex, setCharIndex] = useState(0);
-  const typingSpeed = 100;
+  const typingSpeed = 100; 
 
   useEffect(() => {
     const typeEffect = setInterval(() => {
       if (charIndex < texts[textIndex].length) {
         setCurrentText((prev) => prev + texts[textIndex][charIndex]);
-        setCharIndex(charIndex + 1);
+        setCharIndex((prev) => prev + 1);
       } else {
         clearInterval(typeEffect);
         setTimeout(() => {
           setCharIndex(0);
           setCurrentText('');
-          setTextIndex((prev) => (prev + 1) % texts.length);
-        }, 1000);
+          setTextIndex((prev) => (prev + 1) % texts.length); 
+        }, 1000); 
       }
     }, typingSpeed);
 
-    return () => clearInterval(typeEffect);
+    return () => clearInterval(typeEffect); 
   }, [charIndex, textIndex]);
 
   return (
     <section id="home" className="min-h-screen flex items-center bg-[#121212] text-white">
       <div className="container mx-auto px-6">
         <div className="flex flex-col md:flex-row items-center justify-between">
-          {/* Text Section */}
+          
           <div className="md:w-1/2 mb-8 md:mb-0">
             <h1
               className="text-6xl font-bold mb-4 animate-fade-in-down text-gradient-to-r from-purple-400 to-pink-600"
@@ -47,9 +47,16 @@ export default function Hero() {
               and driving innovation. Eager to collaborate with dynamic teams and advance in impactful development
               projects.
             </p>
+            <br />
+            <a
+              href="/poojatiwari.pdf" 
+              download="My_CV" 
+              className="px-6 py-3 bg-[#121212] text-white rounded-lg border-2 border-gradient-to-r from-purple-400 to-pink-600"
+            >
+              Download CV
+            </a>
           </div>
 
-          {/* Image Section with Border */}
           <div className="relative bg-gradient-to-r from-purple-400 to-pink-600 lg:w-[420px] lg:h-[420px] w-[270px] h-[270px] rounded-full p-[20px] flex items-center justify-center">
             <div className="bg-[#181818] w-full h-full rounded-full overflow-hidden flex items-center justify-center">
               <Image
